de6ce01ae4a4dd1ef8d4bdc633250692bafbd443
Ritu <ritu.basak01@gmail.com>
2024-07-02 16:07:39 +0530

    change5

--- js/script.js
+++ js/script.js
@@ -654,17 +654,25 @@ $(document).ready(function () {
 
 
 
+    function vibrateDevice() {
+        if ('vibrate' in navigator) {
+            navigator.vibrate([200, 100, 200, 100, 200]);
+            alert('Device vibrated with pattern');
+        } else {
+            alert('Vibration API is not supported by this browser');
+        }
+    }
+    $('.bannerImg').click(function(){
+        vibrateDevice();
+    })
+
+
+
+
 });
 
 
 
 
-function vibrateDevice() {
-    if ('vibrate' in navigator) {
-        navigator.vibrate([200, 100, 200, 100, 200]);
-        console.log('Device vibrated with pattern');
-    } else {
-        console.log('Vibration API is not supported by this browser');
-    }
-}
-vibrateDevice();
+
+
